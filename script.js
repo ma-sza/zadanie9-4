bb940ff7a997ed71eba370fc754b30f669bba031
Maria Ulanicka <maria.ulanicka@gmail.com>
2018-03-18 12:06:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function drawTree(heigh) {
-  for (i = 0; i <= heigh; i++) {
-    var star = "";
-    for (j = 0; j <= i; j++) {
-      star += " *";
+function drawTree(height) {
+        // Iterate until we reach desired height
+        for(var level = 0; level < height; level++) {
+
+            // Keep stars here
+            var star = "";
+
+            // How many stars we need
+            var starCount = level * 2 + 1;
+
+            // How many spaces we need
+            var offset = height - level - 1;
+
+            // Add spaces
+            for (var spaces = 0; spaces < offset; spaces++) {
+                star += " ";
+            }
+
+            // Add stars
+            for(var starLine = 0; starLine < starCount; starLine++) {
+                star += "*";
+            }
+
+            console.log(star);
+        }
     }
-    console.log(star);
-  }
-}
-drawTree(5);
+
+    drawTree(10);
